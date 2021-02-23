a406939eaf5e82b33c4c33abd574105b49d8f469
fullstoc <77878598+fullstoc@users.noreply.github.com>
2021-02-23 14:57:30 +0500

    scroll
    scrool qushildi

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,17 @@
+
+
+var headernav = document.querySelector('.header_nav')
+
+window.onscroll = () =>{
+    if(scrollY > 20){
+        headernav.style.background='#0a0a0a'
+        
+    }else{
+        headernav.style.background='transparent'
+
+    }
+}
+
 var typed =new Typed(".typing",{
   strings:["Devoloper", "Designer", "Freelancer"],
   typeSpeed:170,
